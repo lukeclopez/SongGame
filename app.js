dfe1128733a9d624666d14c508ec3453ccec5813
Luke Lopez <xlucas14@gmail.com>
2019-05-21 15:52:40 -0400

    First commit

---
+++ app.js
@@ -0,0 +1,18 @@
+var scores, guessWord, tabooWords;
+var namesEl, scoresEl, guessWordEl, tabooWordsEl;
+
+namesEl = [
+    document.getElementById('name-1'), 
+    document.getElementById('name-2')
+];
+
+scoresEl = [
+    document.getElementById('score-1'), 
+    document.getElementById('score-2')
+];
+
+guessWordEl = document.getElementById('guess-word');
+
+
+scores = [0 , 0];
+
